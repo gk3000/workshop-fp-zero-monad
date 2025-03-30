5917f468a236642a2d897ab4e0b78450a471bd81
Kyle Simpson <getify@gmail.com>
2025-03-30 10:54:05 -0500

    added solution

--- exercise.js
+++ exercise.js
@@ -6,6 +6,28 @@ runTests(collectFilenames);
 
 // *****************************************
 
-function collectFilenames(fs,allowedExtensions) {
-	// TODO: implement per requirements
+function strLower(str) { return str?.toLowerCase(); }
+
+function collectFilenames(fs,allowedExtensions,filenames = []) {
+	if (fs.length == 0) return filenames;
+	if (allowedExtensions != null) {
+		allowedExtensions = allowedExtensions.map(strLower);
+	}
+
+	var [ entry, ...tail ] = fs;
+
+	if (Array.isArray(entry)) {
+		return collectFilenames([ ...entry, ...tail ],allowedExtensions,filenames);
+	}
+	else if (typeof entry == "string") {
+		let entryExt = strLower(entry.match(/\.(.+)$/)?.[1]);
+		if (
+			!allowedExtensions ||
+			allowedExtensions.includes(entryExt)
+		) {
+			filenames = [ ...filenames, entry ];
+		}
+
+		return collectFilenames(tail,allowedExtensions,filenames);
+	}
 }
