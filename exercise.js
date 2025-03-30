4c0c88bd996bfeecd4ff552e65a631aca841fd1c
Kyle Simpson <getify@gmail.com>
2025-03-30 10:49:09 -0500

    added solution

--- exercise.js
+++ exercise.js
@@ -7,5 +7,21 @@ runTests(selectUsers);
 // *****************************************
 
 function selectUsers(users) {
-	// TODO: implement per requirements
+	var result = [];
+
+	for (let i = 0; i < users.length; i++) {
+		let user = users[i];
+		if (user.verified == true) {
+			let newUser = { name: user.name, email: user.email };
+			result.push(newUser);
+		}
+	}
+
+	result.sort((a,b) => (
+		(a.name < b.name) ? -1 :
+		(a.name > b.name) ? 1 :
+		0
+	));
+
+	return result;
 }
